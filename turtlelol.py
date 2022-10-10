74c8dc85e692068a6dfe8cb1fe4aa49ae4580e15
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2022-10-10 11:00:40 +0300

    Add files via upload

---
+++ turtlelol.py
@@ -0,0 +1,51 @@
+#Tõnis Kändmaa
+#10.10.2022
+#KILpKONNN
+import turtle
+
+#tekitan akna ja lisan muutujasse
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("Kilpkonna Harjutus01")
+
+
+
+
+"""
+#konna loomine
+konn1 = turtle.Turtle()
+konn1.color("red")
+konn1.forward(100)
+konn1.left(90)
+konn1.penup() #pliiats üles
+konn1.left(90)
+konn1.forward(100)
+konn1.left(45)
+konn1.pendown() #pliiats alla
+konn1.color("blue")
+konn1.backward(100)
+turtle.exitonclick()
+"""
+
+konn1 = turtle.Turtle()
+konn1.color("lime")
+konn1.left(0)
+konn1.forward(100)
+
+konn2 = turtle.Turtle()
+konn2.color("red")
+konn2.left(45)
+konn2.forward(100)
+
+konn3 = turtle.Turtle()
+konn3.color("orange")
+konn3.left(90)
+konn3.forward(100)
+
+konn4 = turtle.Turtle()
+konn4.color("green")
+konn4.left(135)
+konn4.forward(100)
+
+
+
