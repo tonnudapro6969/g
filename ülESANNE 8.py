13b9b5e353936e64620f8076919eb2e497a01438
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2022-10-10 11:35:04 +0300

    Add files via upload

---
+++ ülESANNE 8.py
@@ -0,0 +1,47 @@
+
+
+
+#Tõnis Kändmaa
+#10.10
+#mingi x ülesanne maeitea  aga jou mario kuidas laheb
+
+import turtle
+aken = turtle.Screen()
+aken.setup(800,800)
+aken.title("ylesanne")
+
+kylg=100
+
+konn1 = turtle.Turtle()
+for x in range(16):
+    konn1.forward(45)
+    konn1.left(45)
+    konn1.forward(45)
+    konn1.right(90)
+    konn1.forward(45)
+    konn1.right(-45)
+    konn1.forward(45)
+    konn1.right(90)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+turtle.exitonclick()
