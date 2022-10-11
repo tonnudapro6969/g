7ff7bc61b9f62686a667f9617ca90644198b289b
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2022-10-11 09:04:29 +0300

    Add files via upload

---
+++ Harjtusu1.py
@@ -0,0 +1,38 @@
+#Tõnis Kändmaa
+#11.10.2022
+#harjtutus1
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print(" ^^ "" ^^ ")
+
+#ruut
+a = int(input("sisesta ruudu külje pikkus: "))
+s = a * a
+print("vastus: ruudu pindala on",s,"cm^2")
+
+
+print("Tere maailm")
+print("Tere maailm")
+print("googogogga")
+print("it\' my life")
+
+enimi = "joe"
+pnimi = "mama"
+nimi =enimi+" "+pnimi
+print(nimi)
+
+nr = 22               #int
+nr2 = 2.365           #float
+print(nr,"+",nr2,"=",nr+nr2)
+
+#kasutajalt sisendi saamine
+kasutajalt = input("lisa siia nr: ")
+print("sa sisestasid:", kasutajalt)
+n1 = int(input("lisa arv1: "))
+n2 = int(input("lisa arv2: "))
+summa = n1+n2
+print(summa)
