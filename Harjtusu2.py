00f396d69777b867971105233e6058d9e4965f6b
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2022-10-11 09:53:26 +0300

    Add files via upload

---
+++ Harjtusu2.py
@@ -0,0 +1,56 @@
+#Tõnis Kändmaa
+#11.10.2022
+#harjtutus2
+
+
+#kutusekulu arvutamine
+
+
+
+#arvususteemid
+hg = input("sisesta sijhgf vmdgi: ")
+hg = bin()
+hg= hex()
+
+
+#ajajteisnenus
+#tais arvuline jagamine //
+#jääk %
+print(72%60)
+
+import math
+
+#faking hureotptpeiuerhsu
+a,b = 16,9
+c = round(math.sqrt(pow(a,2)+pow(b,2)),2)
+print("huporteenus on",c)
+
+#uiutuatatnada
+minutid = int(input("sisesta minutid: "))
+h = minutid//60
+m = 4
+print("vastus on",h,":",m)
+
+
+#pitsa
+inimest = 3
+hind2 = 12.90
+tip = 0.1
+summa4 = (hind2/inimest)*tip
+print("iga inimene maksab:", summa4,"€")
+
+
+
+
+#4
+hind = 36.75
+ale = 0.4
+kogus =3
+summa3 = round(hind+(hind*ale)*kogus,2)
+print(kogus,"toote hind on",summa3,"€")
+
+#kolnurga umbemroot
+a,b,c = 30,30,30
+P=a+b+c
+print("kolmnurga umbermoot on:" ,P)
+
